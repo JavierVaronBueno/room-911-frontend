2432510ae54bae536b8ecf225c1a5592ab00d2ec
javier_varon <javoxdaemon@gmail.com>
2025-03-30 00:58:50 -0500

    added vue-good-table-next dependency

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/main.css';
+import VueGoodTablePlugin from 'vue-good-table-next';
+import 'vue-good-table-next/dist/vue-good-table-next.css';
 
 const app = createApp(App);
 app.use(router);
+app.use(VueGoodTablePlugin);
 app.mount('#app');
